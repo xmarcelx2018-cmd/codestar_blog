0d8c1312d248999d609f811ab6de956c679258a8
xmarcelx2018-cmd <xmarcelx2018@gmail.com>
2026-08-26 16:20:06 +0100

    Add comments to blog

--- blog/models.py
+++ blog/models.py
@@ -12,5 +12,14 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name="blog_posts"
 )
     content = models.TextField()
+    excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
