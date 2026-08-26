b03e2868ab7138cde8dff883aa32944f65bdfd5d
xmarcelx2018-cmd <xmarcelx2018@gmail.com>
2026-08-26 14:49:10 +0100

    Create superuser

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v$5u&u%mo)kl%gr#69om5u!-%da22y$bvfyrcjiux31mj28rc@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
