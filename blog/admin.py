4a33fe52dad8f0d41fa486c74baa495adeb4b237
xmarcelx2018-cmd <xmarcelx2018@gmail.com>
2026-08-27 09:16:49 +0100

    Add post fixtures and load sample data

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
-# Register your models here.
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 admin.site.register(Comment)
