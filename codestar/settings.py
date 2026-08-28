5cde3e548f470932c17b14eaa82469479cba684a
xmarcelx2018-cmd <xmarcelx2018@gmail.com>
2026-08-28 14:55:58 +0100

    Add homepage template and styling

--- codestar/settings.py
+++ codestar/settings.py
@@ -58,10 +58,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'codestar.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
