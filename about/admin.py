5ce0ff83fb1712a32920d672ef824a7719cb81de
xmarcelx2018-cmd <xmarcelx2018@gmail.com>
2026-09-01 18:44:03 +0100

    Add collaboration request form functionality

--- about/admin.py
+++ about/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
-
-from .models import About
 
 
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'updated_on')
-    search_fields = ['title']
     summernote_fields = ('content',)
+
+# Note: admin.ModelAdmin is the standard way of registering
+#       our model with the admin panel. We do it differently
+#       above because we are supplying Summernote fields.
+#       If you want to customise the admin panel view in your
+#       own projects, then inherit from admin.ModelAdmin like
+#       we do below.
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
